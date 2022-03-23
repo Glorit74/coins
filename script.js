5ed8b7141244df3a2393c51e2954c1f5fb09dbfe
Flora <laszlo.t.flora@gmail.com>
2022-03-23 14:32:51 +0100

    :tada:

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-function loadEvent() {
-    // console.log('The page has loaded');
-}
+const data = [
+  {
+    title: 'US Dollar',
+    sub: '$',
+    text: 'USD',
+  },
+  {
+    title: 'Euro',
+    sub: '€',
+    text: 'EUR',
+  },
+  {
+    title: 'Chinese Yuan',
+    sub: '¥',
+    text: 'CNY',
+  },
+  {
+    title: 'Thai Baht',
+    sub: '฿',
+    text: 'THB',
+  },
+  {
+    title: 'British Pound Sterling',
+    sub: '£',
+    text: 'GBP',
+  },
+  {
+    title: 'Ghanaian Cedi',
+    sub: '₵',
+    text: 'GHS',
+  },
+  {
+    title: 'Japanese Yen',
+    sub: '￥',
+    text: 'JPY',
+  },
+  {
+    title: 'Polish Zloty',
+    sub: 'zł',
+    text: 'PLN',
+  },
+];
 
-window.addEventListener("load", loadEvent);
+function loadEvent() {}
+
+window.addEventListener('load', loadEvent);
